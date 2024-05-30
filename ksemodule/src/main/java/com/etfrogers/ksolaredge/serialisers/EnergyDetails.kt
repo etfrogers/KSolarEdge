8bb2c6d27bb9781b08dcd8d44222b6bbff6e0956
Ed Rogers <etfrogers@hotmail.com>
2024-05-30 15:11:51 +0100

    Implement deserialization of meters to map

--- ksemodule/src/main/java/com/etfrogers/ksolaredge/serialisers/EnergyDetails.kt
+++ ksemodule/src/main/java/com/etfrogers/ksolaredge/serialisers/EnergyDetails.kt
@@ -1,8 +1,15 @@
 package com.etfrogers.ksolaredge.serialisers
 
 import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class EnergyDetailsContainer(
@@ -13,17 +20,36 @@ data class EnergyDetailsContainer(
 data class EnergyDetails(
     val timeUnit: String,
     val unit: String,
-    @SerialName("meters") private val metersList: List<RawMeter>
+    @Serializable(with = MeterSerializer::class) val meters: Meters
 ) {
-    init {
-        val meters: Map<String, MeterValues> = metersList.associate { it.type to it.values }
+//    init {
+//        val meters: Map<String, MeterValues> = metersList.associate { it.type to it.values }
+//    }
+}
+
+internal object MeterSerializer : KSerializer<Meters> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
+        "Meter",
+        PrimitiveKind.STRING
+    )
+
+    override fun serialize(encoder: Encoder, value: Meters) {
+        val rawMeters = value.map {
+            RawMeter(it.key, it.value)
+        }
+        encoder.encodeSerializableValue(ListSerializer(RawMeter.serializer()), rawMeters)
+    }
+
+    override fun deserialize(decoder: Decoder): Meters {
+        val rawMeters = decoder.decodeSerializableValue(ListSerializer(RawMeter.serializer()))
+        return rawMeters.associate { it.type to it.values }
     }
 }
 
-typealias MeterValues = List<MeterTimepoint>
+typealias Meters = Map<String, List<MeterTimepoint>>
 
 @Serializable
-data class RawMeter(
+private data class RawMeter(
     val type: String,
     val values: List<MeterTimepoint>
 )
